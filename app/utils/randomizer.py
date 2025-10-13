503f261c2c11137a20ed70b8b7fad36cb8ed0108
stabldev <114811070+stabldev@users.noreply.github.com>
2025-10-13 20:08:27 +0530

    feat: load more colors from file

--- app/utils/randomizer.py
+++ app/utils/randomizer.py
@@ -1,7 +1,14 @@
+import json
+from pathlib import Path
 import random
 
-EXPRESSIONS = ["smile"]
-COLORS = ["#0AB5F1"]
+# app/assets dir
+ASSETS_DIR = Path(__file__).resolve().parent.parent / "assets"
+
+with open(ASSETS_DIR / "expressions.json", "r") as file:
+    EXPRESSIONS = json.load(file)
+with open(ASSETS_DIR / "colors.json", "r") as file:
+    COLORS = json.load(file)
 
 
 def random_avatar(seed: str) -> dict[str, str]:
