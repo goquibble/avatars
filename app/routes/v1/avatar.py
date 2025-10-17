fc87f80941f1b15b15d4dd0e4ead875c0ef029d1
Stabldev <114811070+stabldev@users.noreply.github.com>
2025-10-17 09:00:58 +0530

    feat: add `size` query param for `/png` endpoint (#3)
    Add `size` query parameter for `/png` avatar endpoint extra numeric
    validators like:
    `ge` (greater then or equal to) `1` and `le` (less than or equal to)
    `1024`, and with a default value of `128`.
    
    Closes #1 
    Superseed of #2 
    
    Additional changes:
    * Removed `random` seeding from `generate_avatar` func.
    * Renamed `unimpressed`.svg to `eww` for keeping names short.

--- app/routes/v1/avatar.py
+++ app/routes/v1/avatar.py
@@ -1,5 +1,6 @@
+from typing import Annotated
 import cairosvg
-from fastapi import APIRouter, Response
+from fastapi import APIRouter, Response, Query
 
 from app.utils.randomizer import random_avatar
 from app.utils.svg import generate_avatar
@@ -22,19 +23,22 @@ async def avatar_svg(
     seed: str = "guest", color: str | None = None, expression: str | None = None
 ) -> Response:
     seed, color, expression = resolve_avatar(seed, color, expression)
-    svg_content = generate_avatar(seed=seed, color=color, expression=expression)
+    svg_content = generate_avatar(color=color, expression=expression)
 
     return Response(content=svg_content, media_type="image/svg+xml")
 
 
 @router.get("/avatar/png")
 async def avatar_png(
-    seed: str = "guest", color: str | None = None, expression: str | None = None
+    seed: str = "guest",
+    color: str | None = None,
+    expression: str | None = None,
+    size: Annotated[int | None, Query(ge=1, le=1024)] = 128,
 ) -> Response:
     seed, color, expression = resolve_avatar(seed, color, expression)
-    svg_content = generate_avatar(seed=seed, color=color, expression=expression)
+    svg_content = generate_avatar(color=color, expression=expression)
     png_bytes = cairosvg.svg2png(
-        bytestring=svg_content.encode("utf-8"), output_width=128, output_height=128
+        bytestring=svg_content.encode("utf-8"), output_width=size, output_height=size
     )
 
     return Response(content=png_bytes, media_type="image/png")
