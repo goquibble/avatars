f94c3c3a25153ecfbd1789be3c1e3dd49283e320
stabldev <114811070+stabldev@users.noreply.github.com>
2026-03-05 22:25:20 +0530

    chore: make app compatible with aws lambda

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from mangum import Mangum
 
 from app.constants import CLIENT_CACHE_MAX_AGE
 from app.middleware.client_cache_middleware import ClientCacheMiddleware
@@ -6,5 +7,8 @@ from app.routes.v1 import avatar
 
 app = FastAPI()
 
-app.add_middleware(ClientCacheMiddleware, max_age=CLIENT_CACHE_MAX_AGE)
+app.add_middleware(ClientCacheMiddleware, max_age=CLIENT_CACHE_MAX_AGE)  # ty:ignore[invalid-argument-type]
 app.include_router(avatar.router, prefix="/1.x")
+
+# for aws lambda
+handler = Mangum(app)
